a839fe81e2714b0dc959c7b77a7f2c81e6a1d450
Adam <adam.wlodarczyk1024@gmail.com>
2021-07-01 22:48:28 +0200

    Start transfer PDF wrong func for remove multipage pdf

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import scanpreparation
+# Function to rename multiple files
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
+def preparedata():
+    scanpreparation.splitpdf()
+    scanpreparation.remove_old_files()
+    scanpreparation.renamefiles()
+# Driver Code
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Calling main() function
+    preparedata()
+
--- scanpreparation.py
+++ scanpreparation.py
@@ -1,2 +1,48 @@
 # Place where i make pdf transform
 # i use this dataset https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/DJHVHB
+import os
+from PyPDF2 import PdfFileWriter, PdfFileReader
+
+# place where you have pdf multiscan
+path = "C:\\Users\\Adam\\Documents\\dane do ml\\pdf"
+COUNT = 1
+
+def renamefiles():
+
+    os.chdir(path)
+
+
+    # Function to increment count
+    # to make the files sorted.
+    def increment():
+        global COUNT
+        COUNT = COUNT + 1
+
+    for f in os.listdir():
+        f_name, f_ext = os.path.splitext(f)
+        f_name = "scans" + str(COUNT)
+        increment()
+
+        new_name = '{} {}'.format(f_name, f_ext)
+        os.rename(f, new_name)
+
+
+
+def splitpdf():
+    os.chdir(path)
+    for filename in os.listdir(path):
+
+        inputpdf = PdfFileReader(open(filename, "rb"))
+
+        for i in range(inputpdf.numPages):
+            output = PdfFileWriter()
+            output.addPage(inputpdf.getPage(i))
+            with open("document-{0}-page{1}.pdf".format(filename, i), "wb") as outputStream:
+                output.write(outputStream)
+
+
+def remove_old_files():
+    os.chdir(path)
+    for filename in os.listdir(path):
+        os.remove(path + "\\" + filename)
+
